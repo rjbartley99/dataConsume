abb73f2f4d645913995509a6a3c927bc2abd31fa
rjbartley99 <61232448+rjbartley99@users.noreply.github.com>
2020-04-17 16:27:21 +0100

    Update dataConsume1.vhd

--- dataConsume1.vhd
+++ dataConsume1.vhd
@@ -21,10 +21,10 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use work.common_pack.ALL
+use work.common_pack.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -48,10 +48,13 @@ entity dataConsume is
 end dataConsume;
 
 architecture Behavioral of dataConsume is
+
 type state_type is (IDLE, WAIT_DATA,RECIEVE,LOAD_RIGHT,LOAD_LEFT,DATA_READY);
 signal currentstate,nextstate : state_type;
-signal ctrlIn_delayed, ctrlIn_detected, ctrlOut_reg: std_logic;
-
+signal regCount : integer;
+signal ctrlIn_delayed, ctrlIn_detected, ctrlOut_reg, peakCompare,enable_regCount: std_logic;
+signal peakReg: CHAR_ARRAY_TYPE(6 downto 0);
+signal shiftReg: CHAR_ARRAY_TYPE(3 to 0);
 begin
 receiver_nextState: process(currentState, ctrlIn_detected, start) 
 begin
@@ -71,7 +74,7 @@ begin
         when RECIEVE => 
             if peakCompare <= '1' then
                 nextState <= LOAD_RIGHT;
-            elseif regCount <='3' then
+            else if regCount <=3 then
                 nextState <= LOAD_LEFT;
             else 
                 nextState <= WAIT_DATA;
@@ -83,6 +86,7 @@ begin
                 nextState <= WAIT_DATA;
             end if;
         when LOAD_RIGHT =>
+            enable_regCount<='1';
             if numWordCount <='1' then 
                 nextState <= DATA_READY;
             else
@@ -101,4 +105,40 @@ delay_CtrlIn: process(clk)
   end process;  
 ctrlIn_detected <= ctrlIn xor ctrlIn_delayed;
 
+counter: process(clk)
+begin
+	if rising_edge(sysclk) then
+		if reset ='1' then
+			regCount <= 0;
+		else	
+			if (enable_regCount <= '1') then
+			     if currentState<=RECIEVE then
+			         regCount <= regCount + '1;
+			     else 
+			         regCount <= regCount;
+			     end if;
+			  elsif (reset_regCount <= '1') then
+			     regCount <= '0'
+		      end if;
+		 end if;
+    end if;
+  end process;
+comparator: process(clk) 
+begin   
+    if rising_edge(clk) then
+        if (to_integer(unsigned(shiftReg(3))) > to_integer(unsigned(peakReg(3)))) then 
+            peakCompare <= '1';
+        else 
+            peakCompare <= '0';
+        end if; 
+    end if;
+end process;     
+
+seqdone: process(clk)
+begin 
+
+numWords_bcd1<=TO_INTEGER(unsigned(numWords_bcd(2)));
+numWords_bcd2<=TO_INTEGER(unsigned(numWords_bcd(1)));
+numWords_bcd3<=TO_INTEGER(unsigned(numWords_bcd(0)));
+mag<= numWords_bcd3 + (numWords_bcd2 *10) + (numWords_bcd1*100);
 end Behavioral;
